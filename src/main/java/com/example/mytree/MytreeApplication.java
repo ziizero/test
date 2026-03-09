7e64dbe54c08fde399b6f90b80292c87136dbdb2
jiyeong <jyfullsun42@gmail.com>
2026-03-09 15:11:44 +0900

    프로젝트 백엔드 틀 생성

---
+++ src/main/java/com/example/mytree/MytreeApplication.java
@@ -0,0 +1,13 @@
+package com.example.mytree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytreeApplication.class, args);
+	}
+
+}
